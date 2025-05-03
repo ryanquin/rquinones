17b823ac54bd24fdd04ce59be710c2c7c9c00cb4
Ryan Quinones <ryanquin.02@gmail.com>
2025-05-03 22:32:02 +0800

    Initial Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/logo.jpeg";
+import logo from "../assets/logo.png";
 
 const Header = ({ children }) => {
   const reloadPage = () => {
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/logo.jpeg";
+import logo from "../assets/logo.png";
 
 function Loader() {
   return (
